ffcdcb7291dac0c5132e8c3e6094be2de816f9a2
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2017-12-12 18:11:55 -0800

    Add motors using CANTalonFactory.

--- src/org/usfirst/frc/team4915/powerup/RobotMap.java
+++ src/org/usfirst/frc/team4915/powerup/RobotMap.java
@@ -10,4 +10,12 @@ package org.usfirst.frc.team4915.powerup;
  */
 public class RobotMap {
 	public static final int NUM_CAN_DEVICES = 0; // FIXME: Add the correct number of devices on the CAN bus
+	
+	// FIXME: Set these to the correct value.
+    public static final int DRIVETRAIN_MOTOR_PORT_FOLLOWER = 0;
+    public static final int DRIVETRAIN_MOTOR_PORT_MASTER = 0;
+
+    // FIXME: Set these to the correct value.
+    public static final int DRIVETRAIN_MOTOR_STARBOARD_FOLLOWER = 0;
+    public static final int DRIVETRAIN_MOTOR_STARBOARD_MASTER = 0;
 }
--- src/org/usfirst/frc/team4915/powerup/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team4915/powerup/subsystems/Drivetrain.java
@@ -1,7 +1,11 @@
 package org.usfirst.frc.team4915.powerup.subsystems;
 
 import org.usfirst.frc.team4915.powerup.Logger;
+import org.usfirst.frc.team4915.powerup.RobotMap;
 import org.usfirst.frc.team4915.powerup.commands.StopCommand;
+import org.usfirst.frc.team4915.util.CANTalonFactory;
+
+import com.ctre.CANTalon;
 
 /**
  * The subsystem that controls the Drivetrain.
@@ -18,17 +22,47 @@ public class Drivetrain extends SpartronicsSubsystem {
     // This is public to stop logger proliferation by giving Commands a logger
     public Logger m_logger;
     
+    /*
+     * FIXME: I have the motors set up as if our drivetrain has two port
+     * and two starboard motors, each side connected together. This assumes
+     * that the drivetrain will look like that, because it's just an example
+     * of how to use the new CANTalonFactory and set up these motors.
+     * 
+     * We don't know what our drivetrain will look like. Fix the motors
+     * when we do.
+     */
+    
+    // Port motors
+    private CANTalon m_portMaster;
+    private CANTalon m_portFollower;
+    
+    // Starboard motors
+    private CANTalon m_starboardMaster;
+    private CANTalon m_starboardFollower;
+    
     public Drivetrain()
     {
         m_logger = new Logger("Drivetrain", Logger.Level.DEBUG);
         
-        // Pretty much everything should go here, especially because
-        // certain initializations can throw exceptions.
+        // Pretty much everything should go in the try block, 
+        // because certain initializations can throw exceptions
+        // which we want to print, and because we want m_initalized
+        // to be a correct value.
         try
         {
+            m_portMaster = CANTalonFactory.createDefaultTalon(RobotMap.DRIVETRAIN_MOTOR_PORT_MASTER);
+            m_starboardMaster = CANTalonFactory.createDefaultTalon(RobotMap.DRIVETRAIN_MOTOR_STARBOARD_MASTER);
             
-            // This needs to go at the end. We only set m_initialized
-            // (a field of the parent class) on failure (to false).
+            m_portFollower = CANTalonFactory.createPermanentSlaveTalon(RobotMap.DRIVETRAIN_MOTOR_PORT_FOLLOWER,
+                    RobotMap.DRIVETRAIN_MOTOR_PORT_MASTER);
+            m_starboardFollower = CANTalonFactory.createPermanentSlaveTalon(RobotMap.DRIVETRAIN_MOTOR_STARBOARD_FOLLOWER,
+                    RobotMap.DRIVETRAIN_MOTOR_STARBOARD_MASTER);
+            
+            m_portMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+            m_starboardMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+            
+            // This needs to go at the end. We *don't* set
+            // m_initalized here (we only set it on faliure).
             m_logger.info("initialized successfully");
         }
         catch (Exception e)
