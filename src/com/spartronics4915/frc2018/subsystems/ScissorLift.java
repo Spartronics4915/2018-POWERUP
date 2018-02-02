ba350bbc59d48ac2224782dd954c267ef5dce531
joshgoguen <jrgoguen711@gmail.com>
2018-02-01 16:59:41 -0800

    Scissor Lift Constants

--- src/com/spartronics4915/frc2018/subsystems/ScissorLift.java
+++ src/com/spartronics4915/frc2018/subsystems/ScissorLift.java
@@ -5,13 +5,17 @@ import com.spartronics4915.frc2018.loops.Looper;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import com.spartronics4915.frc2018.Constants;
+
 /**
- * The scissor lift is controlled by pnuematics. It has multiple set positions and variable height.
- * The key thing here is that its system state is the highly variable position of the lifter.
+ * The scissor lift is controlled by pnuematics. It has multiple set positions
+ * and variable height.
+ * The key thing here is that its system state is the highly variable position
+ * of the lifter.
  * This is why the state machine looks different.
  */
 public class ScissorLift extends Subsystem
 {
+
     private static ScissorLift sInstance = null;
 
     public static ScissorLift getInstance()
@@ -26,52 +30,54 @@ public class ScissorLift extends Subsystem
     public enum WantedState
     {
         RETRACTED(0), SWITCH(64), SCALE(128);
-        
+
         int mPosition;
+
         private WantedState(int pos)
         {
             mPosition = pos;
         }
-        
+
         public void setPosition(int pos)
         {
             mPosition = pos;
         }
-        
-        public int getPosition() {
+
+        public int getPosition()
+        {
             return mPosition;
         }
     }
 
     private static final int kPotentiometerAllowedError = 10;
     private Solenoid mScissorLifterSolenoid;
-    private Solenoid mScissorLowerSolenoid; 
+    private Solenoid mScissorLowerSolenoid;
     private Solenoid mScissorBrakeSolenoid;
-    
+
     private int mSystemState = 0;
     private WantedState mWantedState = WantedState.RETRACTED;
-    
+
     // Actuators and sensors should be initialized as private members with a value of null here
-    
+
     private ScissorLift()
     {
         boolean success = true;
 
         // Instantiate your actuator and sensor objects here
         // If !mMyMotor.isValid() then success should be set to false
-        mScissorLifterSolenoid = new Solenoid(Constants.kScissorLifterSolenoidId);
-        mScissorLowerSolenoid = new Solenoid(Constants.kScissorLowerSolenoidId);
+        mScissorLifterSolenoid = new Solenoid(Constants.kScissorUpSolenoidId);
+        mScissorLowerSolenoid = new Solenoid(Constants.kScissorDownSolenoidId);
         mScissorBrakeSolenoid = new Solenoid(Constants.kScissorBrakeSolenoidId);
-        this. mInitialized = true;
         logInitialized(success);
     }
-    
-    private Loop mLoop = new Loop() {
+
+    private Loop mLoop = new Loop()
+    {
 
         @Override
         public void onStart(double timestamp)
         {
-            synchronized(ScissorLift.this)
+            synchronized (ScissorLift.this)
             {
                 mSystemState = 0;
             }
@@ -80,38 +86,39 @@ public class ScissorLift extends Subsystem
         @Override
         public void onLoop(double timestamp)
         {
-            synchronized(ScissorLift.this)
+            synchronized (ScissorLift.this)
             {
-                  if (mWantedState.getPosition() < mSystemState - kPotentiometerAllowedError || mWantedState.getPosition() > mSystemState + kPotentiometerAllowedError)
-                    {
-                         // increase
-                         logNotice ("Starting to Raise");
-                         mScissorLowerSolenoid(false);
-                         mScissorLifterSolenoid(true);
-                    }
-                  else if (mWantedState.getPosition() > mSystemState - kPotentiometerAllowedError || mWantedState.getPosition() < mSystemState + kPotentiometerAllowedError)
-                    {
-                         // decrease
-                         logNotice ("Starting to Lower");
-                         mScissorLifterSolenoid(false);
-                         mScissorLowerSolenoid(true);
-                         
-                         
-                    }
- 
+                if (mWantedState.getPosition() < mSystemState - kPotentiometerAllowedError
+                        || mWantedState.getPosition() > mSystemState + kPotentiometerAllowedError)
+                {
+                    // increase
+                    logNotice("Starting to Raise");
+                    mScissorLowerSolenoid.set(false);
+                    mScissorLifterSolenoid.set(true);
                 }
+                else if (mWantedState.getPosition() > mSystemState - kPotentiometerAllowedError
+                        || mWantedState.getPosition() < mSystemState + kPotentiometerAllowedError)
+                {
+                    // decrease
+                    logNotice("Starting to Lower");
+                    mScissorLifterSolenoid.set(false);
+                    mScissorLowerSolenoid.set(true);
+
+                }
+
             }
-    
+        }
 
         @Override
         public void onStop(double timestamp)
         {
-            synchronized(ScissorLift.this)
+            synchronized (ScissorLift.this)
             {
                 stop();
             }
         }
     };
+
     public void setWantedState(WantedState wantedState)
     {
         mWantedState = wantedState;
@@ -120,7 +127,7 @@ public class ScissorLift extends Subsystem
     @Override
     public void outputToSmartDashboard()
     {
-        
+
     }
 
     @Override
@@ -136,18 +143,18 @@ public class ScissorLift extends Subsystem
     @Override
     public void registerEnabledLoops(Looper enabledLooper)
     {
-      //  enabledLooper.register(mLoop);
+        //  enabledLooper.register(mLoop);
     }
+
     // Called to check if we've reached our desired state
     // Eventually we will want it to check for the tolerance; may be a switch statement or multiple methods checking for different goals
     public synchronized boolean isDone()
     {
         {
-            
-              if (mSystemState == mWantedState)
-                        return true;
-            }
-                return false;
+
+            if (mSystemState == mWantedState.getPosition())
+                return true;
+            return false;
         }
     }
 }
