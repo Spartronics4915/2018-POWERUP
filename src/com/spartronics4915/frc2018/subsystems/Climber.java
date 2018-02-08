2f83723dad5f2df3ef5f0444091e1c3e8d25491f
AustinTSmith <domoking56@gmail.com>
2018-02-07 20:48:57 -0800

    Pushing the climbing code to my repository

--- src/com/spartronics4915/frc2018/subsystems/Climber.java
+++ src/com/spartronics4915/frc2018/subsystems/Climber.java
@@ -1,7 +1,12 @@
 package com.spartronics4915.frc2018.subsystems;
 
+import com.spartronics4915.frc2018.Constants;
 import com.spartronics4915.frc2018.loops.Loop;
 import com.spartronics4915.frc2018.loops.Looper;
+import com.spartronics4915.lib.util.drivers.TalonSRX4915;
+import com.spartronics4915.lib.util.drivers.TalonSRX4915Factory;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 /**
  * The climber is mostly a winch that pulls some ropes attached to the top of the scissor
@@ -22,16 +27,23 @@ public class Climber extends Subsystem
     
     public enum SystemState
     {
-        FIXMEING,
+        CLIMBING,
+        HOLDING,
+        IDLING
     }
 
     public enum WantedState
     {
-        FIXME,
+        CLIMB,
+        HOLD,
+        IDLE
     }
 
-    private SystemState mSystemState = SystemState.FIXMEING;
-    private WantedState mWantedState = WantedState.FIXME;
+    private SystemState mSystemState = SystemState.IDLING;
+    private WantedState mWantedState = WantedState.IDLE;
+    private TalonSRX4915 mWinchPrimary = TalonSRX4915Factory.createDefaultMotor(Constants.kClimberWinchPrimaryMotorId);
+    private TalonSRX4915 mWinchSecondary = TalonSRX4915Factory.createDefaultSlave(Constants.kClimberWinchSecondaryMotorId, Constants.kClimberWinchPrimaryMotorId, false);
+    private DoubleSolenoid mStablizerSoleniod = new DoubleSolenoid(Constants.kClimberStabilizationSolenoidId1, Constants.kClimberStabilizationSolenoidId2);
     
     // Actuators and sensors should be initialized as private members with a value of null here
     
@@ -52,7 +64,7 @@ public class Climber extends Subsystem
         {
             synchronized(Climber.this)
             {
-                mSystemState = SystemState.FIXMEING;
+               setSolenoidReverse();
             }
         }
 
@@ -63,19 +75,26 @@ public class Climber extends Subsystem
             {
                 SystemState newState;
                 switch (mSystemState) {
-                    case FIXMEING:
-                        newState = handleFixmeing();
+                    case HOLDING:
+                        newState = handleHolding();
                         break;
+                    case IDLING:
+                        newState = handleIdling();
+                        break;
+                    case CLIMBING:
+                        newState = handleClimbing();
                     default:
-                        newState = SystemState.FIXMEING;
+                        newState = SystemState.HOLDING;
+                        
                 }
                 if (newState != mSystemState) {
-                    logInfo("Climber state from " + mSystemState + "to" + newState);
+                    logInfo("Climber state from " + mSystemState + "to " + newState);
                     mSystemState = newState;
                 }
             }
         }
 
+        
         @Override
         public void onStop(double timestamp)
         {
@@ -87,11 +106,57 @@ public class Climber extends Subsystem
         
     };
     
-    private SystemState handleFixmeing() {
-        // You should probably be transferring state and controlling actuators in here
-        return SystemState.FIXMEING;
+   private void setSolenoidReverse()
+   {
+       mStablizerSoleniod.set(DoubleSolenoid.Value.kReverse);
+   }
+    
+    private SystemState handleClimbing()
+    {
+        mWinchPrimary.set(1.0);
+        mWinchSecondary.set(1.0); //TODO Implement Smart Dashboard
+        if (mWantedState == WantedState.IDLE) 
+        {
+            mStablizerSoleniod.set(DoubleSolenoid.Value.kReverse);   
+            return SystemState.IDLING;
+        }
+        else if (mWantedState == WantedState.HOLD)
+        {
+            return SystemState.HOLDING;   
+        }
+        else 
+        {
+            return SystemState.CLIMBING;   
+        }
+        
     }
     
+    private SystemState handleIdling()
+    {
+        mWinchPrimary.set(0.0);
+        mWinchSecondary.set(0.0);
+        if (mWantedState == WantedState.CLIMB) {
+            mStablizerSoleniod.set(DoubleSolenoid.Value.kForward);
+            return SystemState.CLIMBING;
+        }
+        else 
+        {
+            return SystemState.IDLING;   
+        }
+    }
+
+    private SystemState handleHolding()
+    {
+        mWinchPrimary.set(0.0);
+        mWinchSecondary.set(0.0);
+        if (mWantedState == WantedState.CLIMB) 
+        {
+            
+            return SystemState.CLIMBING;
+        }
+            return SystemState.HOLDING;
+    }
+
     public void setWantedState(WantedState wantedState)
     {
         mWantedState = wantedState;
