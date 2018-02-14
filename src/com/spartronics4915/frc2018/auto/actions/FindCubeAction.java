c0b9e19f0479d68616bd75af2b144e827e48d6e8
loqoman <darwin.s.clark@gmail.com>
2018-02-13 17:16:30 -0800

    Frogot a few more things for testing

--- src/com/spartronics4915/frc2018/auto/actions/FindCubeAction.java
+++ src/com/spartronics4915/frc2018/auto/actions/FindCubeAction.java
@@ -5,5 +5,32 @@ import com.spartronics4915.frc2018.subsystems.Drive;
 
 public class FindCubeAction implements Action
 {
+    Drive mDrive;
+    
+    @Override
+    public boolean isFinished()
+    {
+        return false;
+    }
+
+    @Override
+    public void update()
+    {
+    }
+
+    @Override
+    public void done()
+    {
+        mDrive.stop();
+    }
+
+    @Override
+    public void start()
+    {
+        mDrive = Drive.getInstance();
+        mDrive.setWantSearchForCube();
+    }
+
+}
 
 }
