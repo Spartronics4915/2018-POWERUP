f7dcd8c9e46648c89bcda228fa41b32359147e02
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2018-02-17 13:07:07 -0800

    Begin correcting path transforms.

--- src/com/spartronics4915/frc2018/paths/DriveToFarSwitchFromAPath.java
+++ src/com/spartronics4915/frc2018/paths/DriveToFarSwitchFromAPath.java
@@ -1,16 +1,26 @@
 package com.spartronics4915.frc2018.paths;
 
+import com.spartronics4915.frc2018.Constants;
+import com.spartronics4915.frc2018.paths.PathBuilder.Waypoint;
+import com.spartronics4915.lib.util.control.Path;
 import com.spartronics4915.lib.util.math.RigidTransform2d;
 
 public class DriveToFarSwitchFromAPath extends DriveToFarSwitchFromCPath
 {
     @Override
-    public RigidTransform2d getStartPose() {
-        return new RigidTransform2d(super.getStartPose().getTranslation(), super.getStartPose().getRotation().inverse()); 
+    public Path buildPath()
+    {
+        return PathBuilder.buildPathFromWaypoints(super.getWaypoints());
+//        for (Waypoint w : super.getWaypoints())
+//        {
+//            
+//        }
     }
     
     @Override
-    public boolean isReversed() {
-        return true;
+    public RigidTransform2d getStartPose()
+    {
+        return new RigidTransform2d(Constants.kFieldHeightTranslation.translateBy(super.getStartPose().getTranslation().inverse()),
+                super.getStartPose().getRotation()); 
     }
 }
