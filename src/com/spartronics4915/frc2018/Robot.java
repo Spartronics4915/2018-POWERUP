89cc6469f368048ceef8b73ed8b57c841b90ad5e
ronan33 <scubaronan@comcast.net>
2018-02-19 13:44:10 -0800

    Remove grabber manual test button

--- src/com/spartronics4915/frc2018/Robot.java
+++ src/com/spartronics4915/frc2018/Robot.java
@@ -371,10 +371,6 @@ public class Robot extends IterativeRobot
                 mGrabber.setWantedState(ArticulatedGrabber.WantedState.PREPARE_INTAKE);
             }
             
-            if (mControlBoard.readButton(Buttons.kTestGrabberManualOpenGrabber))
-            {
-                mGrabber.setWantedState(ArticulatedGrabber.WantedState.MANUAL_OPEN);
-            }
             allButTestPeriodic();
         }
         catch (Throwable t)
